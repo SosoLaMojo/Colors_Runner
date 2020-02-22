ef2fbc7ec6315a688889a9a5632095a8736465ef
Solange <sol.sch1990@gmail.com>
2020-02-22 17:19:30 +0100

    Added script PlayerController with autoRun

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rigidBody;
+    [SerializeField] private float speed;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody.velocity += new Vector2(speed, 0);
+    }
+
+    void Update()
+    {
+        
+    }
+}
