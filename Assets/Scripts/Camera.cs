a563a80c2dc8d6e5efb3787307093b047975cf51
Solange <sol.sch1990@gmail.com>
2020-02-22 17:43:48 +0100

    Added Camera script to follow the player

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x + 5.5f, 0, -10);
+    }
+}
