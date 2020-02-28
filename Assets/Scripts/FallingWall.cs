b8b7e07fc662c218f7160631cadd661d6be74964
Solange <sol.sch1990@gmail.com>
2020-02-28 13:19:23 +0100

    Edit layer for player

--- Assets/Scripts/FallingWall.cs
+++ Assets/Scripts/FallingWall.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class FallingWall : MonoBehaviour
 {
+    private Rigidbody2D rigidBody;
+
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            rigidBody.bodyType = RigidbodyType2D.Dynamic;
+        }
     }
 }
